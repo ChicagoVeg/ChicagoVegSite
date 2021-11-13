287a8983009e28a5b34f92b996dd3943d86210e5
Phil <zoldello@gmail.com>
2021-11-13 12:43:00 -0800

    Update sponsorus.js

--- src/pages/sponsorus.js
+++ src/pages/sponsorus.js
@@ -17,23 +17,18 @@ const SponsorUsPage = () => (
           suggest possible sponsors for our group.
         </p>
         <p className="">
-          Our group offers a great opportunity for local businesses that serve
-          the local community to increase their visibility without hurting their
-          budget. For a little support (that would help cover our monthly and
-          other expenses) local businesses would be able to advertise as a proud
-          sponsor of our community group and, most importantly, they will have a
-          low-cost "targeted" advertisement as we will place the company logo
-          and web site in the sponsorship areas of this and our
-          {' '}
-          <a href="http://www.meetup.com/ChicagoVeg">meetup website </a>
-          {' '}
-          (which
-          currently has over 3,500 members).
+Our group offers a great opportunity for local businesses that serve the local community to increase their visibility without hurting their budget. For 
+a little support (that would help cover our monthly and other expenses) local businesses would be able to advertise as a proud sponsor of 
+the largest Midwest Vegetarian/Vegan community group, as well as to have a low-cost "targeted" advertisement with their company name/logo and 
+website link placed on this website home page.
+We will also introduce our sponsors via <a href="https://www.facebook.com/ChicagoVeg/">ChicagoVeg social media 
+  </a> and encourage support them in our annual email newsletter to all (currently 8000+) 
+<a href="https://www.meetup.com/ChicagoVeg/">ChicagoVeg Meetup</a> members.
         </p>
         <p className="">
-          On the top of these benefits, there are a number of additional
-          opportunities to make the sponsorship even more effective: offering
-          perks, discounts, specials, presentations, and more!
+          On the top of these benefits, there are a number of additional opportunities to make the sponsorship even more effective: presentations, 
+            specials, and more!
+
         </p>
         <p className="">
           Please
